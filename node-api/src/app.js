e8f29c7cb1988e796725cddfba3c0d9507e41381
hudsonp.developer@outlook.com <hudsonp.developer@outlook.com>
2020-03-29 14:01:16 -0300

    deploy

--- node-api/src/app.js
+++ node-api/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const {errors} = require('celebrate');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
--- node-api/src/server.js
+++ node-api/src/server.js
@@ -1,3 +1,5 @@
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('all is ok!');
+});
