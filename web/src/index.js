45cb59865f75733fb691161417ee4e41e606493e
hudsonp.developer@outlook.com <hudsonp.developer@outlook.com>
2020-03-29 15:46:48 -0300

    .

--- web/src/index.js
+++ web/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './global.css';
 
-ReactDOM.render( <React.StrictMode>
-    <App/>
-    </React.StrictMode>,
+ReactDOM.render(<React.StrictMode>
+    <App />
+</React.StrictMode>,
     document.getElementById('root')
 );
