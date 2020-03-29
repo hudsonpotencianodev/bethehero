f99c467d29aefbf0d50d1389f3e9a896d8bdd53f
hudsonp.developer@outlook.com <hudsonp.developer@outlook.com>
2020-03-29 16:01:54 -0300

    url api heroku

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.REACT_APP_LOCALAPI || "https://betheheronodeapi.herokuapp.com/",
 })
 
 export default api;
